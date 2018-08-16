eb72e74b500778b1f44edcf4a5175a464887e140
wichittar ongsakpornnimit <wichittarongsakpornnimit@paimacpro.wifi.bkk.throughwave.com>
2018-08-16 16:51:15 +0700

    add test file

--- test/test-string-calculator.js
+++ test/test-string-calculator.js
@@ -1,4 +1,5 @@
 const chai = require('chai');
+const test = require('./test');
 
 const { expect } = chai;
 
@@ -12,11 +13,16 @@ function itAlwaysBe2() {
   expect(2).to.be.equal(2);
 }
 
+function test1() {
+  expect(test('')).to.be.equal(0);
+}
 // Start Example Behaviors
 function exampleBehaviors() {
   it('should be true', itAlwaysTrue);
   it('should be equal 2', itAlwaysBe2);
+  it('should be equal 0', test1);
 }
+
 
 // Start Describe
 describe('Example', exampleBehaviors);
---
+++ test/test.js
@@ -0,0 +1,8 @@
+function test(a) {
+  if (a === '') {
+    return 0;
+  }
+  return 1;
+}
+
+module.exports = test;
